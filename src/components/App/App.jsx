6351fa9900b0afb68c80a6f7daabc9d76ff2ef06
Andrey <antonov.andrey.10101@gmail.com>
2023-02-02 16:20:09 +0300

    feat: add the ability to change hand selection

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,12 @@
 import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
 import Hand from "../Hand/Hand";
 import Health from "../Health/Health";
 import Option from "../Option/Option";
 
 function App() {
+  const [currentHand, setCurrentHand] = useState("idle");
+
   return (
     <Flex
       justifyContent="center"
@@ -11,6 +14,8 @@ function App() {
       w="100vw"
       h="100vh"
       backgroundColor="#1C2F4F"
+      overflow="hidden"
+      position="relative"
     >
       <Flex
         gap="30px"
@@ -22,16 +27,22 @@ function App() {
           backgroundColor="#5671F5"
           image="1"
           imageSrc="/public/svg/rock_icon.svg"
+          option='rock'
+          setCurrentHand={setCurrentHand}
         />
         <Option
           backgroundColor="#ECA922"
           image="2"
           imageSrc="/public/svg/paper_icon.svg"
+          option='paper'
+          setCurrentHand={setCurrentHand}
         />
         <Option
           backgroundColor="#DD405D"
           image="3"
           imageSrc="/public/svg/scissors_icon.svg"
+          option='scissors'
+          setCurrentHand={setCurrentHand}
         />
       </Flex>
       <Health
@@ -48,18 +59,13 @@ function App() {
         progressColor="green"
         imageSrc="/public/img/superhero.png"
       />
-      <Hand
-        top="0"
+      {/* <Hand
+        top="-30px"
         left="1vw"
         deg={180}
-        imageSrc="/public/svg/male_paper.svg"
-      />
-      <Hand
-        right="1vw"
-        bottom="0"
-        deg={0}
-        imageSrc="/public/svg/male_paper.svg"
-      />
+        imageSrc="/public/svg/male_idle.svg"
+      /> */}
+      <Hand right="1vw" bottom="-30px" deg={0} currentHand={currentHand} />
     </Flex>
   );
 }
--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,6 +1,6 @@
-import { Button, Image} from "@chakra-ui/react";
+import { Button, Image } from "@chakra-ui/react";
 
-function Option({ backgroundColor, imageSrc }) {
+function Option({ backgroundColor, imageSrc, setCurrentHand, option }) {
   return (
     <Button
       borderRadius="50%"
@@ -11,10 +11,11 @@ function Option({ backgroundColor, imageSrc }) {
       h="110px"
       color="black"
       _hover={{
-        backgroundColor: "white"
+        backgroundColor: "white",
       }}
+      onClick={() => setCurrentHand(option)}
     >
-      <Image src={imageSrc} w='100%' h='100%'/>
+      <Image src={imageSrc} w="100%" h="100%" />
     </Button>
   );
 }
